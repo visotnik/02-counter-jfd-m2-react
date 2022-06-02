2ea0f3c73568427b98970bbabd54c1ff24d175af
visotnik <tet129@gmail.com>
2022-06-02 19:36:16 +0300

    release className and style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,30 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [imageUrl, setimageUrl] = useState("https://picsum.photos/200");
 
   // const x = <h1>Zero</h1>;
 
   const formCount = () => {
     return count === 0 ? "Zero" : count;
   };
+
+  const styles = {
+    fontSize: "50px",
+    fontWeight: "bold",
+  };
+
   return (
     <>
-      <img src={imageUrl} alt="" />
-      <span>{formCount()}</span>
-      <button>Increment</button>
+      <span style={styles} className="badge bg-primary m-2">
+        {formCount()}
+      </span>
+      <span
+        style={{ fontSize: "50px", fontWeight: "bold" }}
+        className="badge bg-primary m-2"
+      >
+        Inline
+      </span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
     </>
   );
 };
